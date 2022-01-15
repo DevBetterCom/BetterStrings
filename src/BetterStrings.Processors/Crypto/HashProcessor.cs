b6e3037a7caa215713db5b6e0f1ebef18e8deb0d
Steve Smith <steve@kentsmiths.com>
2022-01-15 13:40:47 -0500

    Add md5 processor and unit tests

---
+++ src/BetterStrings.Processors/Crypto/HashProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace BetterStrings.Processors.Crypto;
+
+// TODO: Create a standard processor base type / interface
+public class HashProcessor
+{
+  public HashProcessor()
+  {
+    // TODO: Inject in the algorithm to use
+  }
+
+  public string Process(string input)
+  {
+    using (var md5 = MD5.Create())
+    {
+      //var result = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+      //return Encoding.UTF8.GetString(result);
+
+      byte[] retVal = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+      StringBuilder sb = new StringBuilder();
+      for (int i = 0; i < retVal.Length; i++)
+      {
+        sb.Append(retVal[i].ToString("x2"));
+      }
+      return sb.ToString();
+    }
+   
+  }
+}
---
+++ tests/BetterStrings.UnitTests/Processors/Crypto/HashProcessor_Process.cs
@@ -0,0 +1,20 @@
+﻿using BetterStrings.Processors.Crypto;
+using Xunit;
+
+namespace BetterStrings.UnitTests.Processors.Crypto
+{
+  public class HashProcessor_Process
+  {
+    [Theory]
+    [InlineData("email", "0c83f57c786a0b4a39efab23731c7ebc")]
+    [InlineData("devBetter", "516609b4a40ed15650ba1b850fdfd91d")]
+    public void ReturnsExpectedHashStringGivenInput(string input, string expectedHash)
+    {
+      var processor = new HashProcessor();
+
+      var result = processor.Process(input);
+
+      Assert.Equal(expectedHash, result);
+    }
+  }
+}
