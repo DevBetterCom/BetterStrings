b0c30bf90d4bd8015c438e8af53f88e604c72661
Matthias <matthias.jost@gmail.com>
2022-01-18 20:03:12 +0100

    branch friendly names

---
+++ src/BetterStrings.Processors/FriendlyName/BranchFriendlyName.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterStrings.Processors
+{
+  public class BranchFriendlyName
+  {
+    public BranchFriendlyName()
+    {
+      // todo implement rules
+      // no special char, no spaces > replace with - instead of space
+      // all lower case
+    }
+
+    public string Process(string input)
+    {
+      return input;
+    }
+  }
+}
