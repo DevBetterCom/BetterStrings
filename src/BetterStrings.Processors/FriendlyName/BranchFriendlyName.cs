fb17e0a3788af17b95345da28af0a0f53409aeac
Matthias <matthias.jost@gmail.com>
2022-02-19 22:25:41 +0100

    Start with code for branch friendly name again based on latest spectre code

--- src/BetterStrings.Processors/FriendlyName/BranchFriendlyName.cs
+++ src/BetterStrings.Processors/FriendlyName/BranchFriendlyName.cs
@@ -4,20 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BetterStrings.Processors
-{
-  public class BranchFriendlyName
-  {
-    public BranchFriendlyName()
-    {
-      // todo implement rules
-      // no special char, no spaces > replace with - instead of space
-      // all lower case
-    }
+namespace BetterStrings.Processors.FriendlyName;
 
-    public string Process(string input)
-    {
-      return input;
-    }
+public class BranchFriendlyNameProcessor
+{
+  public BranchFriendlyNameProcessor()
+  {
+    // todo implement rules
+    // no special char, no spaces > replace with - instead of space
+    // all lower case
+  }
+
+  public string Process(string input)
+  {
+    input = input.Replace("Task ", "task/");
+    input = input.ToLower();
+    input = input.Replace("ä", "ae");
+    input = input.Replace("ö", "oe");
+    input = input.Replace("ü", "ue");
+    input = input.Replace(' ', '-');
+    input = input.Replace(".", string.Empty);
+    input = input.Replace(":", string.Empty);
+
+    input = input.Substring(0, Math.Min(input.Length, 50));
+
+    Console.WriteLine("Suggested Branch input:");
+
+    Console.WriteLine(input);
+
+    return input;
   }
 }
