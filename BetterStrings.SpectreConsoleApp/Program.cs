484a40c5d6efdf68288b2a326d4c861071735a68
Matthias <matthias.jost@gmail.com>
2022-03-19 08:00:09 +0100

    starting implement cli with spectre tools and renamed exe

---
+++ BetterStrings.SpectreConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace SpectreConsoleApp;
+
+
+public static class Program
+{
+  public static int Main(string[] args)
+  {
+    var app = new CommandApp();
+    app.Configure(config =>
+    {
+      config.SetApplicationName("BetterStrings");
+      config.ValidateExamples();
+      config.AddExample(new[] { "run", "--no-build" });
+
+      // Run
+     // config.Add
+ //     config.AddCommand<RunCommand>("run");
+
+    });
+
+    return app.Run(args);
+  }
+}
