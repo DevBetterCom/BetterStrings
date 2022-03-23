884a314d0d719a1b4e4dbed8930c6dd3e0cb481d
Matthias <matthias.jost@gmail.com>
2022-03-23 19:43:49 +0100

    complete change to serilog

--- BetterStrings.SpectreConsoleApp/Program.cs
+++ BetterStrings.SpectreConsoleApp/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System.ComponentModel;
+using Microsoft.Extensions.DependencyInjection;
 using Spectre.Console;
 using Spectre.Console.Cli;
+using Serilog;
+using Serilog.Extensions.Logging;
+using Microsoft.Extensions.Logging;
 
-namespace SpectreConsoleApp;
+namespace BetterStrings.SpectreConsoleApp;
 
 public class ProcessorSettings : CommandSettings
 {
@@ -15,13 +19,17 @@ public class ProcessorSettings : CommandSettings
   [CommandOption("-m|--mode")]
   [DefaultValue("interactive")]
   public string Mode { get; set; }
+
+  [CommandOption("-v|--verbose")]
+  public string LogLevel { get; set; }
 }
 
 public class ProcessorCommand : Command<ProcessorSettings>
 {
+  private static IServiceProvider _serviceProvider;
   public override int Execute(CommandContext context, ProcessorSettings settings)
   {
-    switch(settings.Mode)
+    switch (settings.Mode)
     {
       case "interactive":
       case "i":
@@ -34,8 +42,47 @@ public class ProcessorCommand : Command<ProcessorSettings>
 
     return 0;
   }
+  private Serilog.ILogger CreateLogger(string logLevel)
+  {
+    var loggerLevel = logLevel.ToLower().ToLogEventLevel().Value;
+
+    var loggerConfiguration = new LoggerConfiguration()
+      .MinimumLevel.Is(loggerLevel)
+      .Enrich.FromLogContext()
+      .WriteTo.Console();
+
+    var logger = loggerConfiguration.CreateLogger();
+    Log.Logger = logger;
+
+    logger.Debug("Logger created with level {0}", loggerLevel);
+
+    return logger;
+  }
+
+  private static ServiceProvider SetupDi(ConfigInfo configInfo, Serilog.ILogger logger)
+  {
+    var services = new ServiceCollection()
+          .AddLogging()
+          .AddSingleton<ILoggerFactory>(services => new SerilogLoggerFactory(logger, false))
+          .AddSingleton(configInfo);
+
+    return services.BuildServiceProvider();
+  }
+
   public void EnterInteractiveMode(ProcessorSettings settings)
   {
+    AnsiConsole.Write(new Rule("[blue]Welcome to BetterString[/]"));
+
+    var configInfo = new ConfigInfo(settings.LogLevel);
+
+    var logger = CreateLogger(configInfo.LogLevel);
+
+    logger.Debug("Logger Enabled");
+
+    _serviceProvider = SetupDi(configInfo, logger);
+
+    logger.Debug("DI Setup Done");
+
     AnsiConsole.MarkupLine("[bold blue]Interactive mode...[/]");
 
     string userInputString = AnsiConsole.Ask<string>("Provide a string to transform: ");
@@ -64,7 +111,6 @@ public static class Program
       config.SetApplicationName("BetterStrings");
 
     });
-
     return app.Run(args);
   }
 }
